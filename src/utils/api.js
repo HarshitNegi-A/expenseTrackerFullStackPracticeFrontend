49118815bc08762ee5d9bbfac4a23bc24eb05cf5
Harshit Negi <harshitnegi226@gmail.com>
2025-09-08 22:46:28 +0530

    .

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,3 @@
 // src/utils/api.js
-const BASE_URL = "https://13.127.225.137:3000"; // your EC2 backend URL
+const BASE_URL = "http://13.127.225.137:3000"; // your EC2 backend URL
 export default BASE_URL;
